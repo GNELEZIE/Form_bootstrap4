708f7d1145759da1018149485003d49e5ab67033
Gnelezie <47115156+GNELEZIE@users.noreply.github.com>
2019-04-06 07:32:32 +0200

    Update function.php

--- Form_Boostrap4/php/function.php
+++ Form_Boostrap4/php/function.php
@@ -1,12 +1,9 @@
 <?php
 
-function insertData($db_table,$db_value,$data_value,$db_inconnu,$db){
-    
-    
-       $inser=$db->prepare("INSERT INTO" .$db_table. "(" .$db_value. ") VALUES(" .$db_inconnu. ")");
-       $result=$inser->execute($data_value);
-    
-    return $result;
+function insertData($db_table ,$db_value,$db_inconnu ,$data_value ,$db) {
+    $reqes= $db->prepare("INSERT INTO ".$db_table."(".$db_value.") VALUES(".$db_inconnu.")");
+     $result=$reqes->execute($data_value);
+       return $result;
        
     
     
